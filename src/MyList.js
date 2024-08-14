30609ed06f51af37f6bcddd9461b6d2e7c2cf3ef
vincent anjiri <vincentanjiri12@gmail.com>
2024-08-14 22:04:01 +0300

    initial commkit

---
+++ src/MyList.js
@@ -0,0 +1,13 @@
+function MyList(props) {
+  return (
+    <ul id="concepts">
+      <li className="concept">
+        <img src={props.image} alt={props.title} />
+        <h2>{props.title}</h2>
+        <p>{props.description}</p>
+      </li>
+    </ul>
+  );
+}
+
+export default MyList;
